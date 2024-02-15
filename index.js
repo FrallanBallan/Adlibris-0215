b604a66a03ab1109629c54ddd4592967f7657d2d
Angelocoll <angelo.collocolo@yh.nackademin.se>
2024-02-15 13:58:38 +0100

    push design visual

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-document
-  .querySelector(".hamburger-menu")
-  .addEventListener("click", function () {
-    document.querySelector(".hamburger-menu").classList.toggle("open");
-    document.querySelector(".menu").classList.toggle("open");
+// document
+//   .querySelector(".hamburger-menu")
+//   .addEventListener("click", function () {
+//     document.querySelector(".hamburger-menu").classList.toggle("open");
+//     document.querySelector(".menu").classList.toggle("open");
 
-    let Hamburgermeny = document.querySelector(".hamText");
+//     let Hamburgermeny = document.querySelector(".hamText");
 
-    // Kolla om hamburgermenyn är öppen eller stängd och ändra texten därefter
-    if (Hamburgermeny.textContent === "Stäng") {
-      Hamburgermeny.textContent = "Meny";
-    } else {
-      Hamburgermeny.textContent = "Stäng";
-    }
-  });
+//     // Kolla om hamburgermenyn är öppen eller stängd och ändra texten därefter
+//     if (Hamburgermeny.textContent === "Stäng") {
+//       Hamburgermeny.textContent = "Meny";
+//     } else {
+//       Hamburgermeny.textContent = "Stäng";
+//     }
+//   });
